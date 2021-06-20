db37b2a8bb67665f421922253683413900374843
CSAapps <csasoftwares@gmail.com>
2021-06-20 14:42:54 +0530

    ready to real testing

--- content.js
+++ content.js
@@ -16,26 +16,26 @@ function saveToFile(textToWrite, fileNameToSaveAs) {
     downloadLink.click();
 }
 
-// var ul = document.querySelector('ul.participants-ul');
-// var ulItms = Array.from(ul.querySelectorAll('.participants-item__display-name'));
+var ul = document.querySelector('ul.participants-ul');
+var ulItms = Array.from(ul.querySelectorAll('.participants-item__display-name'));
 
-// var names = ulItms.map(itm => itm.textContent);
-// var date = Date.now();
-// var titile = document.title;
+var names = ulItms.map(itm => itm.textContent);
+var date = Date.now();
+var titile = document.title;
 
-// names = names.filter(name => name.textContent != "Chandani Miss");
-
-// var data = {
-//     "date": date,
-//     // "titile": titile,
-//     "names": names
-// };
+names = names.filter(name => name.textContent != "Chandani Miss");
 
 var data = {
-    "date": Date.now() + (3600 * 1000 * 24) * 2,
-    // "title": "My title",
-    "names": ["හිරුන් නිම්සර 6-B", "Pulmi dahamsa", "CHITHIRA MANTHILA GRADE 6B", "Onitha Jithulaka", "Thenusha Dilshan 6-A", "😎😎😎😎    සහෙන්ස  සත්සර.    😛😛😛😛😛", "6A mithun ransara", "Thinath thajan", "🎀NETHUKI HASARA🎀", "Nimsara Hansamal", "Lasindu Bhagya  6 - B", "Saumya Piumali 6-A", "thisara nethmina6A", "Malithi. chathushika", "Navidu. 🐧😷🦄😍😍🙏👆👨‍👩‍👧‍👧🥀🌹🌷💐🌤️🪐🦄🦜", "Vihagi anjana", "Chanuda  Dissanayake 6B", "Yenuli yohansa", "Vauyni Nemnadi", "Rusiru", "🤫🤫දනංජය 😏😒", "Lusada 6-B", "Chamathka Dewmini Grade 6B", "🙏සත්සර යසස් 6-A🙏", "10G 09 Sadheera Lakdinu", "Keshawa lakshitha 6-A🙏", "Demika", "Manoli", "Methmi Prabarsha 6-B", "Sanuka 6B", "꧁༒SAHIR O𝕟𝕖 𝕤𝕙𝕠𝕥 ༒╾━╤デ╦︻", "--විහග ඉදුසර", "Dharani"]
+    "date": date,
+    // "titile": titile,
+    "names": names
 };
+
+// var data = {
+//     "date": Date.now() + (3600 * 1000 * 24) * 5,
+//     // "title": "My title",
+//     "names": ["හිරුන් නිම්සර 6-B", "Pulmi dahamsa", "CHITHIRA MANTHILA GRADE 6B", "Onitha Jithulaka", "Thenusha Dilshan 6-A", "😎😎😎😎    සහෙන්ස  සත්සර.    😛😛😛😛😛", "6A mithun ransara", "Thinath thajan", "🎀NETHUKI HASARA🎀", "Nimsara Hansamal", "Lasindu Bhagya  6 - B", "Saumya Piumali 6-A", "thisara nethmina6A", "Malithi. chathushika", "Navidu. 🐧😷🦄😍😍🙏👆👨‍👩‍👧‍👧🥀🌹🌷💐🌤️🪐🦄🦜", "Vihagi anjana", "Chanuda  Dissanayake 6B", "Yenuli yohansa", "Vauyni Nemnadi", "Rusiru", "🤫🤫දනංජය 😏😒", "Lusada 6-B", "Chamathka Dewmini Grade 6B", "🙏සත්සර යසස් 6-A🙏", "10G 09 Sadheera Lakdinu", "Keshawa lakshitha 6-A🙏", "Demika", "Manoli", "Methmi Prabarsha 6-B", "Sanuka 6B", "꧁༒SAHIR O𝕟𝕖 𝕤𝕙𝕠𝕥 ༒╾━╤デ╦︻", "--විහග ඉදුසර", "Dharani"]
+// };
 
 chrome.runtime.sendMessage(data, function(success) {
     if (success) {
